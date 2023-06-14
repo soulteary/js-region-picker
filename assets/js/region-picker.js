9001c07da505ed2778399e82a6c90e744e76c907
soulteary <soulteary@gmail.com>
2023-06-15 02:25:35 +0800

    chore: handle area-picker-letter

--- assets/js/region-picker.js
+++ assets/js/region-picker.js
@@ -1,4 +1,9 @@
-// fix the region icon code
+/**
+ * fix the region icon code
+ *
+ * @param {string} code
+ * @returns {string} icon code
+ */
 function regionIconFixer(code) {
   if (code === "uk") {
     return "gb";
@@ -8,18 +13,48 @@ function regionIconFixer(code) {
   return code;
 }
 
-function initRegionPickerLetters() {
-  // area-picker-letter
+/**
+ * Generate the region picker letters template
+ *
+ * @param {array} regionList
+ */
+function initRegionPickerLetters(regionList) {
+  const firstLettersDict = regionList
+    .map((item, _) => {
+      return [item.name.slice(0, 1).toUpperCase(), item.code.slice(0, 1).toUpperCase()];
+    })
+    .reduce((prev, item) => {
+      prev[item[0]] = true;
+      prev[item[1]] = true;
+      return prev;
+    }, {});
+
+  let template = "";
+  for (let i = 65; i <= 90; i++) {
+    const char = String.fromCharCode(i);
+    if (firstLettersDict[char]) {
+      template += `<span>${char} </span>`;
+    } else {
+      template += `<span class="picker-letter-disabled">${char} </span>`;
+    }
+  }
+
+  document.querySelector(".area-picker-letter").innerHTML = template;
 }
 
-function initRegionList() {
-  const template = window.RegionOptions.region.map((item, _) => {
+/**
+ * Generate the region list template
+ *
+ * @param {array} regionList
+ */
+function initRegionList(regionList) {
+  const template = regionList.map((item, _) => {
     const { code, cname, name } = item;
     const icon = regionIconFixer(code);
 
     return `<div class="area-picker-item flex flex-row align-center">
-      <label for="picker-${code}" class="checkBox-inner">
-          <input id="picker-${code}" type="checkbox" name="picker-${code}" value="${code}" />
+      <label for="region-picker-${code}" class="checkBox-inner">
+          <input id="region-picker-${code}" type="checkbox" name="region-picker" value="${code}" />
           <span class="checkBox"></span>
       </label>
       <img src="assets/region-icon/${icon}.svg" alt="${cname}" />
@@ -33,4 +68,5 @@ function initRegionList() {
   document.querySelector("#areaPicker .area-picker-list").innerHTML = template.join("");
 }
 
-initRegionList();
+initRegionPickerLetters(window.RegionOptions.region);
+initRegionList(window.RegionOptions.region);
